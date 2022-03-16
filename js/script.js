81067862ee2f2efe58e8bd3020a0183c9abc80ca
smckelli <smckelli@gmail.com>
2022-03-15 22:20:27 -0500

    reworked character variables

--- js/script.js
+++ js/script.js
@@ -1,30 +1,55 @@
 
-// This challenge was inspired (at least in part) by the Tutorials Tonight blogpost involving a Random Password Generator using JavaScript located at https://www.tutorialstonight.com/password-generator-in-javascript.php and from a post by Grossbard1849 on Reddit at https://www.reddit.com/r/AskProgramming/comments/katbot/javascript_help_with_password_generator/
-
-var passCharUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passCHarLow = "abcdefghijklmnopqrstuvwxyz";
-var passCharNum = "0123456789";
-var passCharSym = "~!@#$%^&*()-=+[]\{}|;:/<>?"
-
-// *** BORROWED!!!! reddit post***
-// var passwordLengthUser = prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
-// passwordLengthUser = parseInt(passwordLengthUser);
-// if (passwordLengthUser < 8) {
-// alert("Password must have more than 7 characters!");
-// return "";
-// }
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var passLength = 8;
+var passArray= {
+  passLower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  passUpper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  passNumber: ["0","1","2","3","4","5","6","7","8","9"],
+  passSymbol: ["~","!","@","#","$","%","^","&","*","(",")","-","=","+"]
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(passArray.passSymbol);
+
+// var passLower= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var passUpper= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// var passNumber= ["0","1","2","3","4","5","6","7","8","9"];
+// var passSymbol= ["~","!","@","#","$","%","^","&","*","(",")","-","=","+"];
+
+// console.log(passSymbol);
+
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// function generatePassword() {
+
+// }
+
+
+
+
+// reference  https://www.youtube.com/watch?v=v2jfGo7ztm8
+
+
+
+
+// This challenge was inspired (at least in part) by the Tutorials Tonight blogpost involving a Random Password Generator using JavaScript located at https://www.tutorialstonight.com/password-generator-in-javascript.php and from a post by Grossbard1849 on Reddit at https://www.reddit.com/r/AskProgramming/comments/katbot/javascript_help_with_password_generator/
